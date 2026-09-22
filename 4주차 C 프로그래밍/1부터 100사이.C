18814dfa8327b7f3c728509ca851a4a2753ec669
BABAM8Aq <156641114+BABAM8Aq@users.noreply.github.com>
2026-09-22 15:57:51 +0900

    Add files via upload

---
+++ 4주차 C 프로그래밍/1부터 100사이.C
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int answer;
+    int num;
+    int count=0;
+
+    srand(time(NULL));
+
+    answer = rand() % 100 + 1;
+
+    printf("1부터 100사이에 숫자를 맞춰보세요!");
+
+    while(1)
+    {
+        printf("숫자 입력:")
+    }
+}
+
+
+
+
+
+
+
+
+    printf("1부터 100사이에 숫자를 맞춰보세요!");
+
+    while(1)
+    {
+        printf("숫자 입력:")}
